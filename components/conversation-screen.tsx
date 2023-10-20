27c1819bcc4176df2045dc453a690761be9588e5
duogenesis <136373989+duogenesis@users.noreply.github.com>
2023-10-20 13:08:20 +1100

    Change it again

--- components/conversation-screen.tsx
+++ components/conversation-screen.tsx
@@ -318,7 +318,7 @@ const ConversationScreen = ({navigation, route}) => {
       >
         {lastMessageStatus === 'timeout' ?  "Message not delivered. Are you online?" : '' }
         {lastMessageStatus === 'blocked' ?  name + ' is unavailable right now. Try messaging someone else!' : '' }
-        {lastMessageStatus === 'not unique' ?  "Intros need to be completely original! Try saying something else..." : '' }
+        {lastMessageStatus === 'not unique' ?  "Someone already used that intro! Intros need to be completely original. Try saying something else..." : '' }
       </DefaultText>
       {!messageFetchTimeout && !isDeletedUser &&
         <TextInputWithButton onPress={onPressSend}/>
