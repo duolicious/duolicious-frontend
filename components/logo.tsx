bd59948a40873622326211df333343057d97e6f4
duogenesis <136373989+duogenesis@users.noreply.github.com>
2024-01-19 01:53:25 +1100

    Welcome screen

---
+++ components/logo.tsx
@@ -0,0 +1,221 @@
+import * as React from 'react';
+import Svg, { G, Rect } from "react-native-svg"
+
+const Logo = ({
+  size = 48,
+  color = 'white',
+}: {
+  size?: number,
+  color?: string,
+}) => {
+  return (
+    <Svg
+       width={size}
+       height={size}
+       viewBox="0 0 4.2333331 4.2333332"
+     >
+      <G>
+        <Rect
+           fill={color} strokeWidth="1" strokeLinecap="round" strokeDashoffset="6.8276"
+           id="rect499"
+           width="0.26458332"
+           height="0.26458332"
+           x="2.3812499"
+           y="1.0583333" />
+        <Rect
+           fill={color} strokeWidth="1" strokeLinecap="round" strokeDashoffset="6.8276"
+           id="rect501"
+           width="0.26458332"
+           height="0.26458332"
+           x="2.6458333"
+           y="1.0583333" />
+        <Rect
+           fill={color} strokeWidth="1" strokeLinecap="round" strokeDashoffset="6.8276"
+           id="rect503"
+           width="0.26458332"
+           height="0.26458332"
+           x="2.1166666"
+           y="1.3229166" />
+        <Rect
+           fill={color} strokeWidth="1" strokeLinecap="round" strokeDashoffset="6.8276"
+           id="rect505"
+           width="0.26458332"
+           height="0.26458332"
+           x="2.1166666"
+           y="1.5875" />
+        <Rect
+           fill={color} strokeWidth="1" strokeLinecap="round" strokeDashoffset="6.8276"
+           id="rect507"
+           width="0.26458332"
+           height="0.26458332"
+           x="2.3812499"
+           y="1.8520833" />
+        <Rect
+           fill={color} strokeWidth="1" strokeLinecap="round" strokeDashoffset="6.8276"
+           id="rect509"
+           width="0.26458332"
+           height="0.26458332"
+           x="2.6458333"
+           y="2.1166666" />
+        <Rect
+           fill={color} strokeWidth="1" strokeLinecap="round" strokeDashoffset="6.8276"
+           id="rect511"
+           width="0.26458332"
+           height="0.26458332"
+           x="2.9104166"
+           y="2.3812499" />
+        <Rect
+           fill={color} strokeWidth="1" strokeLinecap="round" strokeDashoffset="6.8276"
+           id="rect513"
+           width="0.26458332"
+           height="0.26458332"
+           x="3.175"
+           y="2.1166666" />
+        <Rect
+           fill={color} strokeWidth="1" strokeLinecap="round" strokeDashoffset="6.8276"
+           id="rect515"
+           width="0.26458332"
+           height="0.26458332"
+           x="3.4395833"
+           y="1.8520833" />
+        <Rect
+           fill={color} strokeWidth="1" strokeLinecap="round" strokeDashoffset="6.8276"
+           id="rect517"
+           width="0.26458332"
+           height="0.26458332"
+           x="3.7041664"
+           y="1.5875" />
+        <Rect
+           fill={color} strokeWidth="1" strokeLinecap="round" strokeDashoffset="6.8276"
+           id="rect519"
+           width="0.26458332"
+           height="0.26458332"
+           x="3.7041664"
+           y="1.3229166" />
+        <Rect
+           fill={color} strokeWidth="1" strokeLinecap="round" strokeDashoffset="6.8276"
+           id="rect521"
+           width="0.26458332"
+           height="0.26458332"
+           x="2.9104166"
+           y="1.3229166" />
+        <Rect
+           fill={color} strokeWidth="1" strokeLinecap="round" strokeDashoffset="6.8276"
+           id="rect523"
+           width="0.26458332"
+           height="0.26458332"
+           x="3.175"
+           y="1.0583333" />
+        <Rect
+           fill={color} strokeWidth="1" strokeLinecap="round" strokeDashoffset="6.8276"
+           id="rect525"
+           width="0.26458332"
+           height="0.26458332"
+           x="3.4395833"
+           y="1.0583333" />
+        <Rect
+           fill={color} strokeWidth="1" strokeLinecap="round" strokeDashoffset="6.8276"
+           id="rect499-7"
+           width="0.26458332"
+           height="0.26458332"
+           x="0.52916676"
+           y="1.5875" />
+        <Rect
+           fill={color} strokeWidth="1" strokeLinecap="round" strokeDashoffset="6.8276"
+           id="rect501-5"
+           width="0.26458332"
+           height="0.26458332"
+           x="0.79375011"
+           y="1.5875" />
+        <Rect
+           fill={color} strokeWidth="1" strokeLinecap="round" strokeDashoffset="6.8276"
+           id="rect503-3"
+           width="0.26458332"
+           height="0.26458332"
+           x="0.26458347"
+           y="1.8520836" />
+        <Rect
+           fill={color} strokeWidth="1" strokeLinecap="round" strokeDashoffset="6.8276"
+           id="rect505-5"
+           width="0.26458332"
+           height="0.26458332"
+           x="0.26458347"
+           y="2.1166666" />
+        <Rect
+           fill={color} strokeWidth="1" strokeLinecap="round" strokeDashoffset="6.8276"
+           id="rect507-6"
+           width="0.26458332"
+           height="0.26458332"
+           x="0.52916676"
+           y="2.3812499" />
+        <Rect
+           fill={color} strokeWidth="1" strokeLinecap="round" strokeDashoffset="6.8276"
+           id="rect509-2"
+           width="0.26458332"
+           height="0.26458332"
+           x="0.79375011"
+           y="2.6458333" />
+        <Rect
+           fill={color} strokeWidth="1" strokeLinecap="round" strokeDashoffset="6.8276"
+           id="rect511-9"
+           width="0.26458332"
+           height="0.26458332"
+           x="1.0583335"
+           y="2.9104166" />
+        <Rect
+           fill={color} strokeWidth="1" strokeLinecap="round" strokeDashoffset="6.8276"
+           id="rect513-1"
+           width="0.26458332"
+           height="0.26458332"
+           x="1.3229166"
+           y="2.6458333" />
+        <Rect
+           fill={color} strokeWidth="1" strokeLinecap="round" strokeDashoffset="6.8276"
+           id="rect515-2"
+           width="0.26458332"
+           height="0.26458332"
+           x="1.5875"
+           y="2.3812499" />
+        <Rect
+           fill={color} strokeWidth="1" strokeLinecap="round" strokeDashoffset="6.8276"
+           id="rect517-7"
+           width="0.26458332"
+           height="0.26458332"
+           x="1.8520833"
+           y="2.1166666" />
+        <Rect
+           fill={color} strokeWidth="1" strokeLinecap="round" strokeDashoffset="6.8276"
+           id="rect519-0"
+           width="0.26458332"
+           height="0.26458332"
+           x="1.8520833"
+           y="1.8520836" />
+        <Rect
+           fill={color} strokeWidth="1" strokeLinecap="round" strokeDashoffset="6.8276"
+           id="rect521-9"
+           width="0.26458332"
+           height="0.26458332"
+           x="1.0583335"
+           y="1.8520836" />
+        <Rect
+           fill={color} strokeWidth="1" strokeLinecap="round" strokeDashoffset="6.8276"
+           id="rect523-3"
+           width="0.26458332"
+           height="0.26458332"
+           x="1.3229166"
+           y="1.5875" />
+        <Rect
+           fill={color} strokeWidth="1" strokeLinecap="round" strokeDashoffset="6.8276"
+           id="rect525-6"
+           width="0.26458332"
+           height="0.26458332"
+           x="1.5875"
+           y="1.5875" />
+      </G>
+    </Svg>
+  );
+};
+
+export {
+  Logo,
+};
