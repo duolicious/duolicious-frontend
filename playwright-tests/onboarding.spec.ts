238e8511740615d2d8400be7be43a042461ad862
duogenesis <136373989+duogenesis@users.noreply.github.com>
2024-07-24 02:06:21 +1000

    Try HARs again

--- playwright-tests/onboarding.spec.ts
+++ playwright-tests/onboarding.spec.ts
@@ -2,12 +2,9 @@ import { test, expect } from '@playwright/test';
 
 test('has correct onboarding flow', async ({ page }) => {
   await page.route('**', async (route, request) => {
-    // Parse the URL of the request
     const url = new URL(request.url());
 
-    // Check if the port is 8080
     if (url.port === "8080") {
-      // Fulfill or modify the request as needed
       await route.fulfill({
         json: {
           "api_version": 5,
@@ -20,98 +17,8 @@ test('has correct onboarding flow', async ({ page }) => {
     }
   });
 
-  await page.route('*/**/stats', async route => {
-    await route.fulfill({ json: {"num_active_users":1} });
-  });
-
-  await page.route('*/**/request-otp', async route => {
-    await route.fulfill({
-      json: {
-        "session_token": "redacted"
-      }
-    });
-  });
-
-  await page.route('*/**/check-otp', async route => {
-    await route.fulfill();
-  });
-
-  await page.route('*/**/onboardee-info', async route => {
-    await route.fulfill();
-  });
-
-  await page.route('*/**/profile-info', async route => {
-    await route.fulfill({
-      json: {
-        "about": "",
-        "chats": "Immediately",
-        "clubs": [],
-        "drinking": "Unanswered",
-        "drugs": "Unanswered",
-        "education": null,
-        "ethnicity": "Unanswered",
-        "exercise": "Unanswered",
-        "gender": "Woman",
-        "has kids": "Unanswered",
-        "height": null,
-        "hide me from strangers": "No",
-        "intros": "Every 3 days",
-        "location": "Paris, \u00cele-de-France, France",
-        "long distance": "Unanswered",
-        "looking for": "Unanswered",
-        "name": "Test",
-        "occupation": null,
-        "orientation": "Unanswered",
-        "photo": null,
-        "photo_blurhash": null,
-        "photo_verification": null,
-        "relationship status": "Unanswered",
-        "religion": "Unanswered",
-        "show my age": "Yes",
-        "show my location": "Yes",
-        "smoking": "Unanswered",
-        "star sign": "Unanswered",
-        "theme": {
-          "background_color": "#ffffff",
-          "body_color": "#000000",
-          "title_color": "#000000"
-        },
-        "units": "Metric",
-        "verified_age": false,
-        "verified_ethnicity": false,
-        "verified_gender": false,
-        "wants kids": "Unanswered"
-      }
-    });
-  });
-
-  await page.route('*/**/finish-onboarding', async route => {
-    await route.fulfill({
-      json: {
-        "clubs": [],
-        "pending_club": null,
-        "person_id": 13,
-        "person_uuid": "ed2f2e22-d62e-4d29-bada-62e25d6dedcb",
-        "units": "Metric"
-      }
-    });
-  });
-
-  await page.route('*/**/search-locations?q=Paris', async route => {
-    await route.fulfill({
-      json: [
-        "Paris, \u00cele-de-France, France",
-        "Paris, Ontario, Canada",
-        "Paris (Paris 02), \u00cele-de-France, France",
-        "Paris (Od\u00e9on), \u00cele-de-France, France",
-        "Paris, Texas, United States",
-        "Paris (Bercy), \u00cele-de-France, France",
-        "Paris (Ternes), \u00cele-de-France, France",
-        "Paris (Paris 16 Passy), \u00cele-de-France, France",
-        "Paris (Auteuil), \u00cele-de-France, France",
-        "Paris, Illinois, United States"
-      ]
-    });
+  await page.routeFromHAR('./playwright-hars/onboarding.har', {
+    update: false,
   });
 
   await page.goto('http://localhost:8081');
