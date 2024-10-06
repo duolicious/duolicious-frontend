96db776fa648e0260e035393b4329f0ff6382293
duogenesis <136373989+duogenesis@users.noreply.github.com>
2024-10-06 23:12:09 +1100

    Move Q&A filters to the top of the list

--- components/q-and-a-device.tsx
+++ components/q-and-a-device.tsx
@@ -3,20 +3,47 @@ import {
 } from 'react-native';
 import Ionicons from '@expo/vector-icons/Ionicons';
 
-const QAndADevice = ({color, fontSize = 20, isBold = false}) => {
+const QAndADevice = ({
+  color,
+  fontSize = 20,
+  isBold = false,
+  spacing = -6,
+}) => {
   const noIcon = isBold ? 'close-circle' : 'close-circle-outline';
   const yesIcon = isBold ? 'checkmark-circle' : 'checkmark-circle-outline';
 
   return (
     <View style={{flexDirection: 'row'}}>
-      <Ionicons
-        style={{color: color, fontSize: fontSize}}
-        name={noIcon}
-      />
-      <Ionicons
-        style={{color: color, fontSize: fontSize, marginLeft: -3}}
-        name={yesIcon}
-      />
+      <View
+        style={{
+          backgroundColor: 'white',
+          borderRadius: 999,
+          overflow: 'hidden',
+        }}
+      >
+        <Ionicons
+          style={{color: color, fontSize: fontSize}}
+          name={noIcon}
+        />
+      </View>
+      <View
+        style={{
+          backgroundColor: 'white',
+          borderRadius: 999,
+          overflow: 'hidden',
+          marginLeft: spacing,
+        }}
+      >
+        <Ionicons
+          style={{
+            color: color,
+            fontSize: fontSize,
+            flexShrink: 1,
+            overflow: 'hidden',
+          }}
+          name={yesIcon}
+        />
+      </View>
     </View>
   );
 };
