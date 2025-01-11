4d219fbc064979a35b81b7785642114ba184631f
DK <99230888+DeepeshKalura@users.noreply.github.com>
2025-01-11 17:12:25 +0000

    Formatter configuration Fixes #296
    add reactive native and tsx files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,30 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
-
+import reactNativePlugin from "eslint-plugin-react-native";
+import tsParser from "@typescript-eslint/parser";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+  {
+    files: [
+      "**/*.{js,mjs,cjs,ts,tsx}"
+    ],
+    ignores: ["**/*.config.mjs"],
+    languageOptions: {
+      globals: globals.browser,
+      parser: tsParser,
+    },
+    plugins: {
+      "react-native": reactNativePlugin,
+      "react": reactPlugin,
+      "@typescript-eslint": tseslint,
+    },
+    rules: {
+      ...pluginJs.configs.recommended.rules,
+      ...tseslint.configs.recommended.rules,
+      ...reactPlugin.configs.recommended.rules,
+      ...reactNativePlugin.configs.recommended.rules,
+    },
+  }
 ];
