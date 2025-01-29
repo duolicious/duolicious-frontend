94b47b5391346fc95502d4f0e904481080cc27b3
duogenesis <136373989+duogenesis@users.noreply.github.com>
2025-01-29 16:53:04 +1100

    Fix linting errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,4 +2,18 @@
 module.exports = {
   extends: 'expo',
   ignorePatterns: ['/dist/*'],
+  rules: {
+    "@typescript-eslint/no-empty-object-type": "off",
+    "@typescript-eslint/no-redeclare": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      {
+        "argsIgnorePattern": "^_$",
+        "varsIgnorePattern": "^_$",
+        "caughtErrorsIgnorePattern": "^_$"
+      }
+    ],
+    "react-hooks/exhaustive-deps": "off",
+    "react/display-name": "off",
+  }
 };
