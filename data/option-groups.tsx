7af78440d3fe00a4e1e5bc76284e6218144d5256
duogenesis <136373989+duogenesis@users.noreply.github.com>
2025-06-08 03:06:47 +1000

    verb order

--- data/option-groups.tsx
+++ data/option-groups.tsx
@@ -1173,7 +1173,7 @@ const createAccountOptionGroups: OptionGroup<OptionGroupInputs>[] = [
     title: 'Step 4 of 5: Birth Date',
     description: (
       "Unlock better matches and features exclusively for age-verified users " +
-      "by setting this accurately. It can’t be easily changed, so make sure " +
+      "by setting this accurately. It can’t be changed easily, so make sure " +
       "it’s correct."
     ),
     input: {
