331bdb6c3453a08e175d0cc7f1308a2c165d6a77
duogenesis <136373989+duogenesis@users.noreply.github.com>
2025-11-09 18:17:07 +1100

    Version 32.0.1

--- app.config.ts
+++ app.config.ts
@@ -6,7 +6,7 @@ import { ExpoConfig } from 'expo/config';
 const config: ExpoConfig = {
   name: 'Duolicious',
   slug: 'duolicious',
-  version: "32.0.0",
+  version: "32.0.1",
   orientation: "portrait",
   icon: './assets/icon.png',
   newArchEnabled: true,
